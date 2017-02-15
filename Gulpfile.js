28bcad743f8d5e9d68b7108979ff5ce9abc4f337
Bogdan Arabadzhi <bogdan.today@gmail.com>
2017-02-15 23:07:58 +0300

    Add building task to gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,15 @@
 const gulp = require('gulp');
+
 const connect = require('gulp-connect');
+
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+
+const minify = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+
+const inlinecss = require('gulp-inline-css');
+const inlinesource = require('gulp-inline-source');
 
 gulp.task('connect', function() {
   connect.server({
@@ -21,6 +31,21 @@ gulp.task('css', function() {
 gulp.task('watch', function() {
   gulp.watch(['src/*.html'], ['html']);
   gulp.watch(['src/css/*.css'], ['css']);
+});
+
+gulp.task('build', function() {
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpif('*.css', minify()))
+    .pipe(gulpif('*.html', inlinesource()))
+    .pipe(gulpif('*.html', inlinecss({
+      removeStyleTags: false
+    })))
+    .pipe(gulpif('*.html', htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    })))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['connect', 'watch']);
